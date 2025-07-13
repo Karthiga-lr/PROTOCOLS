8ed6161b3809250df6d7d195c72094087b2a1f92
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-14 00:18:19 +0530

    Update spi_slave.v

--- OFF-CHIP PROTOCOLS/SPI/spi_slave.v
+++ OFF-CHIP PROTOCOLS/SPI/spi_slave.v
@@ -1,1 +1,33 @@
+module spi_slave (
+    input CS,
+    input SCLK,
+    input MOSI,
+    input [7:0] data_in,
+    output reg MISO,
+    output reg [7:0] data_out
+);
+    reg [7:0] tx_shift_reg, rx_shift_reg;
+    reg [2:0] bit_cnt;
+
+    always @(negedge CS) begin
+        tx_shift_reg <= data_in;
+        bit_cnt <= 3'd7;
+    end
+
+    always @(posedge SCLK) begin
+        if (!CS) begin
+            rx_shift_reg[bit_cnt] <= MOSI;
+        end
+    end
+
+    always @(negedge SCLK) begin
+        if (!CS) begin
+            MISO <= tx_shift_reg[bit_cnt];
+            if (bit_cnt == 0)
+                data_out <= rx_shift_reg;
+            else
+                bit_cnt <= bit_cnt - 1;
+        end
+    end
+endmodule
 
